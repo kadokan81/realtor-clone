42f883872f39d25962425912d7466bfe42f7df61
kadokan81 <kadokan81@gmail.com>
2023-01-12 00:00:25 +0100

    add functionality to create and save new listing in firebase storage and database

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,27 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify' /* cspell: disable-line */;
+import Spinier from '../components/Spinier';
+import {
+	getStorage,
+	ref,
+	uploadBytesResumable,
+	getDownloadURL,
+} from 'firebase/storage';
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+} from 'firebase/firestore' /* cspell: disable-line */;
+import { db } from '../firebase';
+import { v4 as uuidv4 } from 'uuid' /* cspell: disable-line */;
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router';
 
 export default function CreateListing() {
+	const auth = getAuth();
+	const navigate = useNavigate();
+	const [geolocationEnabled, setGeolocationEnabled] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const [formData, setFormData] = useState({
 		type: 'rent',
 		name: '',
@@ -13,6 +34,9 @@ export default function CreateListing() {
 		offer: false,
 		regularPrice: 0,
 		discountedPrice: 0,
+		latitude: 55.7,
+		longitude: 9.5,
+		images: {},
 	});
 	const {
 		type,
@@ -26,12 +50,111 @@ export default function CreateListing() {
 		offer,
 		regularPrice,
 		discountedPrice,
+		latitude,
+		longitude,
+		images,
 	} = formData;
-	function onChange() {}
+	function onChange(e) {
+		let boolean = null;
+		if (e.target.value === 'true') {
+			boolean = true;
+		}
+		if (e.target.value === 'false') {
+			boolean = false;
+		}
+		if (e.target.files) {
+			setFormData((prevState) => ({
+				...prevState,
+				images: e.target.files,
+			}));
+		}
+		if (!e.target.files) {
+			setFormData((prevState) => ({
+				...prevState,
+				[e.target.id]: boolean ?? e.target.value,
+			}));
+		}
+	}
+	async function onSubmit(e) {
+		e.preventDefault();
+		setLoading(true);
+
+		if (+discountedPrice >= +regularPrice) {
+			toast.error('Discount price need to bee less than regular price');
+			setLoading(false);
+			return;
+		}
+		if (images.length > 6) {
+			toast.error('maximum 6 images are allowed');
+			setLoading(false);
+			return;
+		}
+		async function storeImage(image) {
+			return new Promise((resolve, reject) => {
+				const storage = getStorage();
+				const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+				const storageRef = ref(storage, filename);
+				const uploadTask = uploadBytesResumable(storageRef, image);
+				uploadTask.on(
+					'state_changed',
+					(snapshot) => {
+						// Observe state change events such as progress, pause, and resume
+						// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+						const progress =
+							(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+						console.log('Upload is ' + progress + '% done');
+						switch (snapshot.state) {
+							case 'paused':
+								console.log('Upload is paused');
+								break;
+							case 'running':
+								console.log('Upload is running');
+								break;
+						}
+					},
+					(error) => {
+						// Handle unsuccessful uploads
+						reject(error);
+					},
+					() => {
+						// Handle successful uploads on complete
+						// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+						getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+							resolve(downloadURL);
+						});
+					}
+				);
+			});
+		}
+
+		const imgUrls = await Promise.all(
+			[...images].map((image) => storeImage(image))
+		).catch((error) => {
+			setLoading(false);
+			toast.error('Images not uploaded');
+			return;
+		});
+
+		const formDataCopy = {
+			...formData,
+			imgUrls,
+			timestamp: serverTimestamp(),
+			userRef: auth.currentUser.uid,
+		};
+		delete formDataCopy.images;
+		!formDataCopy.offer && delete formDataCopy.discountedPrice;
+		const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
+		setLoading(false);
+		toast.success('Listing created');
+		navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+	}
+	if (loading) {
+		return <Spinier />;
+	}
 	return (
 		<main className='max-w-md mx-auto px-2'>
 			<h1 className='text-3xl text-center mt-6 font-bold'>Create Listing</h1>
-			<form>
+			<form onSubmit={onSubmit}>
 				<p className='text-lg mt-6 font-semibold'>Sell/Rent</p>
 				<div className='flex gap-6'>
 					<button
@@ -42,7 +165,7 @@ export default function CreateListing() {
                         shadow-md rounded hover:shadow-lg 
                         focus:shadow-lg active:shadow-lg transition 
                         duration-150 ease-in-out w-full ${
-													type === 'rent'
+													type !== 'sale'
 														? 'bg-white text-black'
 														: 'bg-slate-600 text-white'
 												}`}>
@@ -56,7 +179,7 @@ export default function CreateListing() {
                         shadow-md rounded hover:shadow-lg 
                         focus:shadow-lg active:shadow-lg transition 
                         duration-150 ease-in-out w-full ${
-													type === 'sell'
+													type !== 'rent'
 														? 'bg-white text-black'
 														: 'bg-slate-600 text-white'
 												}`}>
@@ -110,6 +233,7 @@ export default function CreateListing() {
 				<p className='text-lg mt-6 font-semibold'>Parking spot</p>
 				<div className='flex gap-6'>
 					<button
+						type='button'
 						id='parking'
 						value={true}
 						onClick={onChange}
@@ -124,6 +248,7 @@ export default function CreateListing() {
 						yes
 					</button>
 					<button
+						type='button'
 						id='parking'
 						value={false}
 						onClick={onChange}
@@ -175,13 +300,48 @@ export default function CreateListing() {
 					className='w-full font-medium text-sm uppercase 
                         shadow-md rounded hover:shadow-lg 
                         focus:shadow-lg active:shadow-lg transition 
-                        duration-150 ease-in-out '
+                        duration-150 ease-in-out 
+						mb-4'
 					value={address}
 					placeholder='Address'
 					required
 					id='address'
 					onChange={onChange}
 				/>
+				{!geolocationEnabled && (
+					<div className='flex gap-6 '>
+						<div className='w-full'>
+							<p className='text-lg font-semibold'>Latitude</p>
+							<input
+								type='number'
+								id='latitude'
+								value={latitude}
+								onChange={onChange}
+								required
+								min='-90'
+								max='90'
+								className='w-full px-4 py-2 text-xl text-gray-700 bg-white 
+								border border-gray-300 rounded transition duration-150 ease-in-out
+								 focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
+							/>
+						</div>
+						<div className='w-full'>
+							<p className='text-lg font-semibold'>Longitude</p>
+							<input
+								type='number'
+								id='longitude'
+								value={longitude}
+								onChange={onChange}
+								required
+								className='w-full px-4 py-2 text-xl text-gray-700 bg-white border
+								 border-gray-300 rounded transition duration-150 ease-in-out
+								  focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
+								min='-180'
+								max='180'
+							/>
+						</div>
+					</div>
+				)}
 				<p className='text-lg mt-6 font-semibold'>Description</p>
 				<textarea
 					type='text'
@@ -250,7 +410,7 @@ export default function CreateListing() {
 								className='w-[50%] px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center'
 								type='number'
 								value={discountedPrice}
-								id='discount price'
+								id='discountedPrice'
 								onChange={onChange}
 								min='50'
 								max='40000000'
