387a99fe43d88b2651b5d8f02ab31a18866a9bb3
kadokan81 <kadokan81@gmail.com>
2023-01-19 15:13:06 +0100

    create listing page and add Swiper to show image sliders

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -112,6 +112,8 @@ export default function CreateListing() {
 							case 'running':
 								console.log('Upload is running');
 								break;
+							default:
+								break;
 						}
 					},
 					(error) => {
---
+++ src/pages/Listing.jsx
@@ -0,0 +1,73 @@
+import { doc, getDoc } from 'firebase/firestore' /* cspell: disable-line */;
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import { toast } from 'react-toastify' /* cspell: disable-line */;
+import Spinier from '../components/Spinier';
+import { db } from '../firebase';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import SwiperCore, {
+	Autoplay,
+	EffectFade,
+	Navigation,
+	Pagination,
+} from 'swiper';
+
+export default function Listing() {
+	const [listing, setListing] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const params = useParams();
+	const navigate = useNavigate();
+	SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+	useEffect(() => {
+		setLoading(true);
+		async function fetchListing() {
+			const docRef = doc(db, 'listings', params.listingId);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				setListing(docSnap.data());
+
+				setLoading(false);
+			} else {
+				navigate('/');
+				toast.error('Listing does not exist');
+			}
+		}
+		fetchListing();
+	}, [navigate, params.listingId]);
+
+	if (loading && listing === null) {
+		return <Spinier />;
+	}
+	return (
+		<div>
+			<Swiper
+				modules={[Navigation, Pagination, Autoplay, EffectFade]}
+				spaceBetween={50}
+				slidesPerView={1}
+				navigation
+				autoplay={{ delay: 3000 }}
+				effect='fade'
+				pagination={{ type: 'progressbar' }} /* cspell: disable-line */
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={(swiper) => console.log(swiper)}>
+				{listing &&
+					listing.imgUrls.map((url, ind) => (
+						<SwiperSlide key={ind}>
+							<div
+								className='w-full overflow-hidden h-[300px]'
+								style={{
+									background: `url(${url}) center no-repeat`,
+								}}></div>
+						</SwiperSlide>
+					))}
+			</Swiper>
+		</div>
+	);
+}
