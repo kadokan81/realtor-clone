99e57614d30b81cedbb16f891636c2303b0ca43f
kadokan81 <kadokan81@gmail.com>
2023-01-20 02:40:42 +0100

    add share cope functionality with popup message

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,6 +6,7 @@ import Spinier from '../components/Spinier';
 import { db } from '../firebase';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { FaShare } from 'react-icons/fa';
 
 // Import Swiper styles
 
@@ -19,6 +20,7 @@ import 'swiper/css/pagination';
 export default function Listing() {
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(false);
+	const [shareLinkCopy, setShareLinkCopied] = useState(false);
 	const params = useParams();
 	const navigate = useNavigate();
 
@@ -43,7 +45,7 @@ export default function Listing() {
 		return <Spinier />;
 	}
 	return (
-		<div>
+		<main className='max-w-[1980px] mx-auto relative'>
 			<Swiper
 				modules={[Navigation, Pagination, Autoplay, EffectFade]}
 				spaceBetween={50}
@@ -59,13 +61,37 @@ export default function Listing() {
 					listing.imgUrls.map((url, ind) => (
 						<SwiperSlide key={ind}>
 							<div
-								className='w-full overflow-hidden h-[300px]'
+								className='w-full mw overflow-hidden h-[300px]'
 								style={{
 									background: `url(${url}) center no-repeat`,
+									backgroundSize: 'cover',
 								}}></div>
 						</SwiperSlide>
 					))}
 			</Swiper>
-		</div>
+			<div
+				className='w-12 h-12 bg-white  absolute top-[13%] right-[3%] 
+				border-2 border-gray-400  
+				 cursor-pointer rounded-full
+				  z-20 flex items-center justify-center
+				   opacity-70 hover:opacity-100 transition duration-150 ease-in-out'
+				onClick={() => {
+					navigator.clipboard.writeText(window.location.href);
+					setShareLinkCopied(true);
+					setTimeout(() => {
+						setShareLinkCopied(false);
+					}, 2000);
+				}}>
+				<FaShare
+					color='blue'
+					className='text-lg text-slate-500 hover:text-slate-800 transition duration-150 ease-in-out'
+				/>
+			</div>
+			{shareLinkCopy && (
+				<div className='absolute text-blue-600 top-[30%] right-[6%] font-semibold border-2 border-gray-400 rounded-md bg-white opacity-70 z-10 px-2 py-1 '>
+					Link Copied
+				</div>
+			)}
+		</main>
 	);
 }
